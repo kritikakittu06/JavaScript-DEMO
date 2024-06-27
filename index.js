07322b920d5feecb62a6664ca8680f4c5d643074
Kritikakittu2206 <np05cp4a220059@iic.edu.np>
2024-06-27 12:19:51 +0545

    javascript file edited

---
+++ index.js
@@ -0,0 +1,33 @@
+let day = 3;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  default:
+    console.log("Another day");
+}
+
+//for loop
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+
+//while loop
+
+let k = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+//Do-while loop
+let g = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
+
